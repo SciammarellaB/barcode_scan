20e46996db21d060fc722c39d5c27dc5c96e7463
BrennoSciammarella <brenno@giomtecnologia.com.br>
2023-06-07 16:42:27 -0300

    Inicial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <barcode-scaner @onLoad="onLoad($event)" @onDecode="onDecode($event)" @onError="onError($event)"/>
+
+    <span>SCANER BARCODE</span> <br/><br/>
+    <span>{{ scaneado }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HomeView extends Vue {
+  scaneado: string = "";
+  onLoad(val: any){
+    alert("LOAD: " + val);
+  }
+  onDecode(val: any){
+    alert("LOAD: " + val);
+  }
+  onError(val: any){
+    alert("LOAD: " + val);
+  }
+}
+</script>
